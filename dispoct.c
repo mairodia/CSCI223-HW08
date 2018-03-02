55c3a15219fdc8094239c861e8968805a445c275
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-03-01 17:34:40 -0800

    Create dispoct.c

---
+++ dispoct.c
@@ -0,0 +1,18 @@
+// =================================================================================
+// File: dispoct.c
+// =================================================================================
+// Programmer: Jennifer King
+// Project: Multiple Modules
+// Date: 2/28/2018
+// Professor: Scott Edwards
+// Class: CSCI 223F | TR 4:30PM
+// Description:
+//              This function recieves a base 10 int and displays it in base 8.
+// =================================================================================
+#include <stdio.h>
+#include "dispoct.h"
+
+void DispOct(int myInt)
+{
+    printf("The value in octal is 0%o\n", myInt);
+}
