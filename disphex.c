ee24943a447678cdc6c66063f9f4bbbce6f0bad9
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-03-01 17:32:47 -0800

    Create disphex.c

---
+++ disphex.c
@@ -0,0 +1,19 @@
+// =================================================================================
+// File: disphex.c
+// =================================================================================
+// Programmer: Jennifer King
+// Project: Multiple Modules
+// Date: 2/28/2018
+// Professor: Scott Edwards
+// Class: CSCI 223F | TR 4:30PM
+// Description:
+//              This function translates base 10 ints into base 16.
+// =================================================================================
+
+#include <stdio.h>
+#include "disphex.h"
+
+void DispHex(int myInt)
+{
+    printf("The value in hexadecimal is 0x%x\n", myInt);
+}
