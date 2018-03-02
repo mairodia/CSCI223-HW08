2ecb2ab49af4b3051d43ce7647f67db1ee2cbda0
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-03-01 17:33:35 -0800

    Create disphex.h

---
+++ disphex.h
@@ -0,0 +1,15 @@
+// =================================================================================
+// File: disphex.h
+// =================================================================================
+// Programmer: Jennifer King
+// Project: Multiple Modules
+// Date: 2/28/2018
+// Professor: Scott Edwards
+// Class: CSCI 223F | TR 4:30PM
+// Description:
+//              This file contains the protoype for the DispHex function.
+// =================================================================================
+
+#include <stdio.h>
+
+void DispHex(int myInt);
