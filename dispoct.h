57aa9fb16d1b6387b11363ece61ff342132df19a
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-03-01 17:34:11 -0800

    Create dispoct.h

---
+++ dispoct.h
@@ -0,0 +1,15 @@
+// =================================================================================
+// File: dispoct.h
+// =================================================================================
+// Programmer: Jennifer King
+// Project: Multiple Modules
+// Date: 2/28/2018
+// Professor: Scott Edwards
+// Class: CSCI 223F | TR 4:30PM
+// Description:
+//              Contains the prototype for the DispOct function
+// =================================================================================
+
+#include <stdio.h>
+
+void DispOct(int myInt);
